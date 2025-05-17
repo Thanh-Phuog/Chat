e1d772b971aa8b168640d1b83a628f0e06dd9d5a
MTPE <mtpe2301@gmail.com>
2025-05-17 14:51:35 +0700

    Update

---
+++ src/app/chat/[roomId]/page.tsx
@@ -0,0 +1,11 @@
+import ChatScreen from "@/components/screens/chat/ChatScreen";
+
+const ChatPage = ({ params }: { params: { roomId: string } }) => {
+  const { roomId } = params;
+
+  return (
+    <div>
+      <ChatScreen roomId={roomId} />
+    </div>
+  );
+};
